f9a2d8c6d4682dd9c6fdfbdce1a22b7e98fea599
ecourse <smkang@codenuri.co.kr>
2024-08-26 11:10:06 +0900

    Add files via upload

--- DAY1/3_가상함수1.cpp
+++ DAY1/3_가상함수1.cpp
@@ -29,8 +29,16 @@ int main()
 // p->cry() 를 실제 어떤 함수와 연결할것인가 ?
 // => 함수 바인딩(function binding )
 
-// 1. static binding :
+// 1. static binding : 컴파일러가 컴파일 시간에 함수 호출을 결정
+//						컴파일러는 p가 가리키는 실제 객체의 종류는
+//						알수 없다.
+//						단지, p가 Animal* 라는 것만 알수 있다.
+//						"Animal Cry" 호출
 
-
-// 2. dynamic binding : 
+// 2. dynamic binding : 컴파일 시간에는 "p가 가리키는 객체를 조사"하는
+//						기계어 코드를 생성하고
+//						실행할때 메모리를 조사해서 
+//						실제 객체가 어떤 타입인지에 따라 호출 결정
+//						객체가 "Dog" 타입이면
+//						"Dog Cry" 호출
 
