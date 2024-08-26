d09951f9895bf8ae7ac6632ffe77648b4a53fccb
ecourse <smkang@codenuri.co.kr>
2024-08-26 13:16:46 +0900

    Add files via upload

---
+++ DAY1/4_추상클래스1.cpp
@@ -0,0 +1,28 @@
+﻿// 1_추상클래스1.cpp - 10page
+
+// 추상 클래스 : 순수가상함수가 한개 이상 있는 클래스
+// 특징 : 객체를 만들수 없다.
+// 의도 :
+class Shape
+{
+public:
+	virtual void draw() = 0; // 순수 가상함수( pure virtual function)
+							 // 특징 : 구현부가 없고, "= 0" 으로 표기							 
+};
+class Rect : public Shape
+{
+	// draw() 의 구현부를 제공하지 않으면 Rect 도 추상(abstract)
+	// draw() 의 구현부를 제공하면 추상이 아님(concrete)
+public:
+	void draw() override {}
+};
+int main()
+{
+	Shape  s;  // error
+	
+	Rect  r;   // ok. 
+
+	Shape* p;
+}
+
+
