c67be47ddc6625b1b73007e1a0b3608cd3f65e86
ecourse <smkang@codenuri.co.kr>
2024-08-26 09:14:30 +0900

    Add files via upload

--- DAY1/1_생성자1.cpp
+++ DAY1/1_생성자1.cpp
@@ -4,20 +4,33 @@
 class Base
 {
 public:
-	Base() { std::cout << "Base()" << std::endl; }
+	Base()     { std::cout << "Base()" << std::endl; }
 	Base(int a){ std::cout << "Base(int)" << std::endl; }
 	~Base()    { std::cout << "~Base()" << std::endl; }
 };
+
 class Derived : public Base
 {
-public:						
-	Derived()		{ std::cout << "Derived()" << std::endl; }
-	Derived(int a)	{ std::cout << "Derived(int)" << std::endl; }
-	~Derived()		{ std::cout << "~Derived()" << std::endl; }
+public:				
+	// 사용자 코드		// 컴파일러가 변경한 코드
+	Derived()			// Derived() : Base() 
+	{ 
+		std::cout << "Derived()" << std::endl; 
+	}
+	Derived(int a)		// Derived(int a) : Base()	<= 핵심 : 항상 Base의
+						//				디폴트 생성자를 호출하는 코드 추가
+	{ 
+		std::cout << "Derived(int)" << std::endl; 
+	}
+	~Derived()	
+	{ 
+		std::cout << "~Derived()" << std::endl; 
+		// Base::~Base()
+	}
 };
+
 int main()
 {
-//	Derived d1;
-	Derived d2(5);
-
+//	Derived d1;     // call Derived::Derived()
+	Derived d2(5);  // call Derived::Derived(int)
 }
