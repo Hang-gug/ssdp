71b3c055f0732f860e028d95658e4bfcf2a874b8
ecourse <smkang@codenuri.co.kr>
2024-08-26 16:18:48 +0900

    Add files via upload

---
+++ DAY1/6_Edit2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+// Validation 정책은 사용자가 변경할수 있어야 합니다.
+
+class Edit
+{
+	std::string data;
+public:
+	std::string get_data()
+	{
+		data.clear();
+
+		while (1)
+		{
+			char c = _getch();
+
+			if (c == 13) break; 
+
+			if (isdigit(c))
+			{
+				data.push_back(c);
+				std::cout << c;
+			}
+		}
+		std::cout << "\n";
+		return data;
+	}
+};
+
+int main()
+{
+	Edit e;
+	while (1)
+	{
+		std::cout << e.get_data() << std::endl;
+	}
+}
+
+
