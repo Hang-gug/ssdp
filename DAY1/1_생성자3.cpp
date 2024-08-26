a3b7c03a7ea713068e12ae9e8b4208369d361ad5
ecourse <smkang@codenuri.co.kr>
2024-08-26 09:25:15 +0900

    Add files via upload

---
+++ DAY1/1_생성자3.cpp
@@ -0,0 +1,20 @@
+﻿// 1_생성자2.cpp
+#include <string>
+#include <iostream>
+
+class Person
+{
+	std::string name;
+	int age;
+public:
+	Person(const std::string& name, int age) : name(name), age(age) {}
+};
+
+int main()
+{
+	Person p2("kim", 30); // ok. 반드시 초기화해야만 객체 생성 가능
+
+//	Person p1;	// 이렇게 객체를 만들수 있다는 것이 좋을까요 ? (초기화 되지 않은 객체)
+}
+
+
