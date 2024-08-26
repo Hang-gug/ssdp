865c45d64a8edc23b98341b646360b682b2ee5f6
ecourse <smkang@codenuri.co.kr>
2024-08-26 13:24:41 +0900

    Add files via upload

--- DAY1/4_추상클래스2.cpp
+++ DAY1/4_추상클래스2.cpp
@@ -32,6 +32,25 @@ int main()
 	p.use_camera(&hc); // ??
 }
 
+// OCP 규칙
+
+// 기능 확장에는 열려 있고 (Open, 새로운 클래스/모듈이 추가되어도)
+// 코드 수정에는 닫혀 있어야(Close, 기존 코드는 수정되지 않도록 )
+// 만들어야 한다는 이론(Principle)
+
+// Open Close Principle ( 개방 폐쇄의 법칙 )
+
+// 객체지향 S/W 설계의 5개 원칙 : SOLID
+
+// SRP
+// OCP
+// LSP
+// ISP
+// DIP
+
+
+
+
 
 
 
