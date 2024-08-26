ac8fddd8a3b9a783a174321c1d1577ac211e4808
ecourse <smkang@codenuri.co.kr>
2024-08-26 13:29:54 +0900

    Add files via upload

---
+++ DAY1/4_추상클래스3.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+
+// #1. 카메라와 카메라 사용자가 지켜야 하는 규칙을 먼저 설계 합니다.
+// 규칙 : 모든 카메라는 아래 클래스로 부터 파생 되어야 한다.
+
+class ICamera
+{
+public:
+	virtual void take() = 0;
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Camera
+{
+public:
+	void take() { std::cout << "Take Picture" << std::endl; }
+};
+
+class HDCamera
+{
+public:
+	void take() { std::cout << "Take HD Picture" << std::endl; }
+};
+
+class People
+{
+public:
+	void use_camera(Camera* p) { p->take(); }
+	void use_camera(HDCamera* p) { p->take(); }
+};
+
+int main()
+{
+	People p;
+	Camera c;
+	p.use_camera(&c);
+
+
+	HDCamera hc;
+	p.use_camera(&hc); // ??
+}
+
+
+
+
+
+
+
+
+
