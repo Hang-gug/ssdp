8fc2348284efebdda5f6eb23a7ce808ad88abc34
ecourse <smkang@codenuri.co.kr>
2024-08-26 09:27:21 +0900

    Add files via upload

--- DAY1/1_생성자3.cpp
+++ DAY1/1_생성자3.cpp
@@ -10,9 +10,16 @@ public:
 	Person(const std::string& name, int age) : name(name), age(age) {}
 };
 
+// 1. Person 에서 파생된 Student 만들어 보세요
+// 2. "int id" 추가해 보세요
+// 3. Student 생성자 만들어 보세요
+// 4. main 에서 Student 객체 생성해 보세요
+
+
 int main()
 {
 	Person p2("kim", 30); // ok. 반드시 초기화해야만 객체 생성 가능
+
 
 //	Person p1;	// 이렇게 객체를 만들수 있다는 것이 좋을까요 ? (초기화 되지 않은 객체)
 }
