9e81e01df365edaed65d9fbf556178951e123c72
ecourse <smkang@codenuri.co.kr>
2024-08-26 16:15:19 +0900

    Add files via upload

---
+++ DAY1/6_Edit1.cpp
@@ -0,0 +1,41 @@
+﻿// 33 page ~ 
+#include <iostream>
+#include <string>
+#include <conio.h>
+
+// Edit 의 입력값을 Validation 하는 기능 추가
+class Edit
+{
+	std::string data;
+public:
+	std::string get_data()
+	{
+		data.clear();
+
+		while (1)
+		{
+			char c = _getch();
+
+			if (c == 13) break; // enter 키 누른 경우
+
+			if (isdigit(c))
+			{
+				data.push_back(c);
+				std::cout << c;
+			}
+		}
+		std::cout << "\n";
+		return data;
+	}
+};
+
+int main()
+{
+	Edit e;
+	while (1)
+	{
+		std::cout << e.get_data() << std::endl;
+	}
+}
+
+
