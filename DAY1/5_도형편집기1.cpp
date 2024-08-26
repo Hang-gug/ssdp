4577651ff179f61de2a9b056af790ea12c8d24c7
ecourse <smkang@codenuri.co.kr>
2024-08-26 14:08:41 +0900

    Add files via upload

---
+++ DAY1/5_도형편집기1.cpp
@@ -0,0 +1,18 @@
+﻿// 15page ~ 
+#include <iostream>
+#include <vector>
+
+// #1. 모든 도형을 타입화 하면 편리합니다.
+
+class Rect
+{
+//	int x, y, w, h, 생성자 등은 생략
+public:
+	void draw() { std::cout << "draw Rect\n"; }
+};
+
+int main()
+{
+
+}
+
