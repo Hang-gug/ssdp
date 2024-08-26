f72c96aa19fcc95bdc292f0835200ae2789b98d8
ecourse <smkang@codenuri.co.kr>
2024-08-26 11:35:56 +0900

    Add files via upload

--- DAY1/3_가상함수2.cpp
+++ DAY1/3_가상함수2.cpp
@@ -23,4 +23,15 @@ int main()
 	// => 실행해서 결과 확인해 보세요
 	Base* p = new Derived;
 	delete p;				
+
+	// 1. 소멸자 호출도 결국 함수 호출입니다.
+	// 2. 소멸자가 "virtual" 이면 : dynamic binding
+	//		   "non-virtual"     : static binding
+	// 
+	// 3. 컴파일러는 "p가 가리키는 곳의 타입은 알수 없고"
+	//    "p자체의 타입(Base*)" 만 알수 있습니다
+
+	// 4. 따라서, 컴파일 할때
+	// => Base의 소멸자가 "virtual 인지 아닌지"를 조사후 binding 결정
+
 }	
