b30c04f930dc9aaf4c63c247769a6a89db67baf4
ecourse <smkang@codenuri.co.kr>
2024-08-26 16:23:19 +0900

    Add files via upload

--- DAY1/6_Edit2.cpp
+++ DAY1/6_Edit2.cpp
@@ -8,6 +8,16 @@ class Edit
 {
 	std::string data;
 public:
+	// 공통성과 가변성의 분리
+	// => 변하지 않은 코드 내부에서 변해야 하는 곳이 있다면
+	//    분리되어야 한다.
+	// => 변하는 것을 별도의 가상함수로  분리한다.
+
+	virtual bool validate(char c)
+	{
+		return true;
+	}
+
 	std::string get_data()
 	{
 		data.clear();
@@ -18,7 +28,7 @@ public:
 
 			if (c == 13) break; 
 
-			if (isdigit(c))
+			if ( validate(c) ) // 값의 유효성 확인(변하는것)을 가상함수에 의존
 			{
 				data.push_back(c);
 				std::cout << c;
