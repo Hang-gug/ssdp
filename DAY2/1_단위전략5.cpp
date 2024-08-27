f6283ed2a316af9a3ae28b49aea00ec302e90807
ecourse <smkang@codenuri.co.kr>
2024-08-27 10:10:05 +0900

    Add files via upload

---
+++ DAY2/1_단위전략5.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+
+// STL 컨테이너에 전달할 메모리 할당기 만들기
+
+// 1. 반드시 템플릿일 필요는 없지만, 다양한 타입에 대해서
+//    사용해야 하므로 일반적으로는 template 으로 작성
+
+template<typename T> 
+class debug_alloc
+{
+public:
+
+};
+
+
+int main()
+{
+	std::vector<int, debug_alloc<int> > v;
+
+	std::cout << "-----------------" << std::endl;
+
+	v.resize(4);
+
+	std::cout << "-----------------" << std::endl;
+
+	v.resize(8);
+
+	std::cout << "-----------------" << std::endl;
+}
