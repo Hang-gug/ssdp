8563c44d7a8866ec4cd8d4c17c207267198e5de0
ecourse <smkang@codenuri.co.kr>
2024-08-27 10:21:15 +0900

    Add files via upload

--- DAY2/1_단위전략5.cpp
+++ DAY2/1_단위전략5.cpp
@@ -29,9 +29,7 @@ public:
 	// 위 2개 위에 아래 3개도 필요 합니다.
 	// => 관례적인 코드이므로 항상 복사해서 사용하면 됩니다.
 	using value_type = T;
-
 	debug_alloc() {}
-
 	template<typename U> debug_alloc(const debug_alloc<U>&) {}
 							// generic 생성자 라는 개념. 
 };
@@ -43,11 +41,19 @@ int main()
 
 	std::cout << "-----------------" << std::endl;
 
-	v.resize(4);
+	v.resize(4); // 이순간 4개 버퍼 할당
 
 	std::cout << "-----------------" << std::endl;
 
-	v.resize(8);
+	v.resize(8); // 1. 8개 할당
+				 // 2. 4개 버퍼내용 => 8개 버퍼에 복사
+				 // 3. 4개 버퍼 제거
+
+	std::cout << "-----------------" << std::endl;
+
+	v.resize(6);	// 이순간 어떻게 동작할까요 ?
+					// 1. 6개 메모리 할당하고, 8개는 제거
+					// 2. 그냥, 8개 계속 사용하고, size 변수만 6으로
 
 	std::cout << "-----------------" << std::endl;
 }
