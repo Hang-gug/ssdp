d3e6b38109a8bc3074179d70b15f63534f898355
ecourse <smkang@codenuri.co.kr>
2024-08-27 16:09:11 +0900

    Add files via upload

---
+++ DAY2/4_Decorator5.cpp
@@ -0,0 +1,32 @@
+﻿// 아래 매크로는 VS 에서 fopen 사용시 나오는 경고 제거
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+class FileStream
+{
+	FILE* file;
+public:
+	FileStream(const char* s, const char* mode = "wt")
+	{
+		file = fopen(s, mode);
+	}
+	~FileStream() { fclose(file); }
+
+	void write(const std::string& s) 
+	{
+		printf("%s 쓰기\n", s.c_str() );
+	}
+};
+
+int main()
+{
+	FileStream fs("a.txt");
+	fs.write("hello");
+
+	NetworkStream ns("100.100.100.100", 3000);
+	ns.write("hello");
+
+	PipeStream ps("my named pipe");
+	ps.write("hello");
+
+}
