be72931b691d0221e17a881212b9579a5d9a5dde
ecourse <smkang@codenuri.co.kr>
2024-08-27 10:02:29 +0900

    Add files via upload

--- DAY2/1_단위전략4.cpp
+++ DAY2/1_단위전략4.cpp
@@ -63,6 +63,7 @@ int main()
 
 
 // strategy vs policy base design
+// => 둘다, 클래스가 사용하는 알고리즘(정책)을 다른 클래스로 분리 하는 것
 
 // strategy : 일반적으로 정책 클래스를 "인터페이스"를 통해서 교체
 
@@ -71,6 +72,19 @@ int main()
 
 
 //						성능					유연성
-// strategy			  : 
+// strategy			  : 느리다(가상함수)		실행시간 교체 가능
+//											e.set_validator(&v1)
+//											e.set_validator(&v2)
 
-// policy base design : 
+// policy base design : 빠르다(인라인도가능)	실행시간 교체할수 없다.
+//											객체 생성시만 템플릿 인자로 전달가능
+
+
+// 디자인 패턴 : 대부분에 객체지향 언어에서 구현 가능한 일반적인 디자인 기술
+//			    GoF's design pattern 23개가 가장 유명하고
+//				이후 등장하는 추가적인  패턴들도 있음(MVC, MVVM 등...)
+// => template method, strategy 등...
+
+// IDioms : 각 언어의 특징을 고려한 디자인 기법
+//			(C++ IDioms, Java Idioms 등... )
+//			구글에서 "C++ Idioms" 검색후 1번째 링크
