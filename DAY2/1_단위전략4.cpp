e4dd53db78ce1d04c136dbbb6aabc7aaf1d0b638
ecourse <smkang@codenuri.co.kr>
2024-08-27 09:48:46 +0900

    Add files via upload

--- DAY2/1_단위전략4.cpp
+++ DAY2/1_단위전략4.cpp
@@ -3,42 +3,45 @@
 
 // 방법 #3. policy base design(단위전략) 을 사용해 봅시다.
 
+// 인터페이스
 
-template<typename T>
-struct IAllocator
-{
-	virtual T* allocate(std::size_t sz) = 0;
-	virtual void deallocate(T* p, std::size_t sz) = 0;
+// 장점 : 약한 결합을 통해서 교체 가능한 설계가 가능하다.
 
-	virtual ~IAllocator() {}
-};
+// 단점 : 결국 가상함수이므로 메모리 오버헤드와 호출시 성능 저하가 있다
 
 
-template<typename T>
+
+template<typename T, typename Alloc>
 class vector
 {
 	T* buff = nullptr;
 	int size;
-	IAllocator<T>* alloc;
+
+//	MyAllocator<T> alloc; // allocator 이름을 직접 사용
+						  // 강한 결합. 교체 불가능.
+
+	Alloc alloc; // 템플릿 인자로 메모리 할당기 타입을 전달
+				 
+
 public:
-	vector(int sz, IAllocator<T>* p) : size(sz), alloc(p)
+	vector(int sz) : size(sz)
 	{
-		buff = alloc->allocate(sz);
+		buff = alloc.allocate(sz);
 	}
 	~vector()
 	{
-		alloc->deallocate(buff, size);
+		alloc.deallocate(buff, size);
 	}
 };
 
-template<typename T> class MyAllocator : public IAllocator<T>
+template<typename T> class MyAllocator 
 {
 public:
-	T* allocate(std::size_t sz) override
+	T* allocate(std::size_t sz) 
 	{
 		return static_cast<T*>(malloc(sizeof(T) * sz));
 	}
-	void deallocate(T* p, std::size_t sz) override
+	void deallocate(T* p, std::size_t sz)
 	{
 		free(p);
 	}
@@ -46,8 +49,7 @@ public:
 
 int main()
 {
-	MyAllocator<int> ma;
-	vector<int> v(4, &ma); 
+	vector<int, MyAllocator<int> > v(4); 
 }
 
 
