485f7393e12183990b870a795f0f8829568527a1
ecourse <smkang@codenuri.co.kr>
2024-08-27 11:10:40 +0900

    Add files via upload

--- DAY2/2_메뉴3.cpp
+++ DAY2/2_메뉴3.cpp
@@ -19,16 +19,38 @@ public:
 	}
 };
 
+class PopupMenu
+{
+	std::string title;
+	std::vector<MenuItem*> v;
+public:
+
+	PopupMenu(const std::string& title) : title(title) {}
+
+	void add(MenuItem* m) { v.push_back(m); }
+
+	// main 의 주석을 참고해서 아래 코드를 생각해 보세요
+	void command()
+	{
+
+	}
+};
+
 int main()
 {
 	MenuItem m1("김밥", 11);
 	MenuItem m2("라면", 12);
 
 	PopupMenu pm("오늘의 점심 메뉴");
+
 	pm.add(&m1);
 	pm.add(&m2);
 
 	pm.command();  // 팝업메뉴 선택시 동작을 생각해 보세요
+					// 1. 김밥
+					// 2. 라면
+					// 3. 종료
+					// 메뉴를 선택하세요 >>
 }
 
 
