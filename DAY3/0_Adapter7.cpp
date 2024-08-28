66890b4bab01f43163083c453a39aeef0902cbea
ecourse <smkang@codenuri.co.kr>
2024-08-28 09:22:23 +0900

    Add files via upload

--- DAY3/0_Adapter7.cpp
+++ DAY3/0_Adapter7.cpp
@@ -10,12 +10,17 @@ class stack
 {
 	C c;
 public:
-	void push(const T& e) { c.push_back(e); };
-
-	void pop() { c.pop_back(); };
-	T& top() { return c.back(); };
+	inline void push(const T& e) { c.push_back(e); };
+	inline void pop() { c.pop_back(); };
+	inline T& top() { return c.back(); };
 };
 
+#include <stack> // C++ 표준 stack 헤더
+				// 이안의 코드가 위에 있는 코드와 거의 유사 합니다.
+				// 흔히 "stack adapter" 라고 부릅니다.
+				// 아래 코드에서 "stack" 대신 "std::stack" 으로 변경하면됩니다.
+				// cppreference.com 에서 "container library" 찾아보세요
+		
 int main()
 {
 	stack<int, std::list<int>>  s1; // list 를 stack 처럼 사용하겠다는 의미
