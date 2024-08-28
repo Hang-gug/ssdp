f162084e3264ed3ff0b764d7094e88dc92de0f84
ecourse <smkang@codenuri.co.kr>
2024-08-28 10:56:46 +0900

    Add files via upload

--- DAY3/CalcProxy.cpp
+++ DAY3/CalcProxy.cpp
@@ -12,3 +12,11 @@ public:
 	int Add(int a, int b) { return ec_send_server(server, 1, a, b); }
 	int Sub(int a, int b) { return ec_send_server(server, 2, a, b); }
 };
+
+// 클라이언트 제작자는 위 클래스 이름을 알수도 없고, 알필요도 없고, 사용해도 안됩니다.
+// DLL 내부에서 약속된 함수를 통해서 객체를 생성해야 합니다.
+
+ICalc* create()
+{
+	return new Calc;
+}
