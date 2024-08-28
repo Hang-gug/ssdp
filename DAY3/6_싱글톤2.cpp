1fc601dcaea83afda4da69730e3cd07f1c4c4aba
ecourse <smkang@codenuri.co.kr>
2024-08-28 15:56:18 +0900

    Add files via upload

---
+++ DAY3/6_싱글톤2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+
+// 오직 한개의 객체를 어느 메모리에 어떻게 만들것인가 ?
+
+// #1. static 지역변수 - mayer's singleton
+
+// #2. static 멤버 데이타
+
+// #3. heap 
+
+class Cursor
+{
+private:
+	Cursor() {}
+
+	Cursor(const Cursor&) = delete; 
+	Cursor& operator=(const Cursor&) = delete;
+
+//	Cursor instance; // error
+	static Cursor instance; // ok
+
+public:
+	static Cursor& get_instance()
+	{
+//		static Cursor instance; 
+		return instance;
+	}
+};
+Cursor Cursor::instance; // C++ 에서는 static 멤버 데이타는 외부 정의 필요
+
+
+int main()
+{
+	Cursor& c1 = Cursor::get_instance();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
