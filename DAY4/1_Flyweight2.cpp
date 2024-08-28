a6b3120f34db7e8ee7b403a35a575d248c30c918
ecourse <smkang@codenuri.co.kr>
2024-08-29 08:46:40 +0900

    Add files via upload

--- DAY4/1_Flyweight2.cpp
+++ DAY4/1_Flyweight2.cpp
@@ -14,14 +14,27 @@ class Image
 public:
 	void Draw() { std::cout << "Draw " << image_url << std::endl; }
 
+	static std::map<std::string, Image*> image_map;
 
 	// 자신의 객체를 만드는 static 멤버 함수
 	static Image* Create(const std::string& url)
 	{
-		Image* img = new Image(url);
+		Image* img = nullptr;
+
+		auto it = image_map.find(url);
+
+		if (it == image_map.end()) // map 에 없는 경우
+		{
+			img = new Image(url);
+			image_map[url] = img;
+		}
+		else
+		{
+			img = it->second;  // map 은 pair 보관. 
+							   // first 는 key, second 는 value(Image)
+		}
 		return img;
 	}
-
 };
 
 int main()
