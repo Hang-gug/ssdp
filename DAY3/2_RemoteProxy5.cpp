e9c3d46e77dd344c6ad416cdd4fe03b3274f8671
ecourse <smkang@codenuri.co.kr>
2024-08-28 11:21:55 +0900

    Add files via upload

--- DAY3/2_RemoteProxy5.cpp
+++ DAY3/2_RemoteProxy5.cpp
@@ -7,10 +7,15 @@
 int main()
 {
 	ICalc* calc = load_proxy();	// 1. 이순간 결국 DLL 의 create() 호출합니다.
-								// 2. new Calc로 생성됩니다.
-								// => "delete"가 필요 하지 않을까요 ?
+								// 2. "new Calc" 로 생성됩니다.
+								// => "delete" 가 필요 하지 않을까요 ?
 
-	delete calc; // 이렇게 해도 될까요 ?				
+	delete calc; // 이렇게 해도 될까요 ?		
+				// 동적모듈(DLL) 에서 할당한 자원(new)은
+				// 동적모듈(DLL) 에서 반납(delete) 해야 합니다.
+
+				// DLL 과 exe 만들때 컴파일러가(또는 버전이) 다를수도 있습니다.
+				// => 다음 단계에서 해결
 
 }
 
