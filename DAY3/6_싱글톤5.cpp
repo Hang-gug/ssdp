4da67754fa62c621acf5a116add792e3894fa92c
ecourse <smkang@codenuri.co.kr>
2024-08-28 16:31:45 +0900

    Add files via upload

--- DAY3/6_싱글톤5.cpp
+++ DAY3/6_싱글톤5.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <mutex>
 
-// 스레드 동기화 이야기.
+// RAII
+// 자원의 할당/해지는 반드시 생성자/소멸자에 의존해라
+
+template<typename T> class lock_guard
+{
+	T& mtx;
+public:
+	lock_guard(T& m) : mtx(m) { mtx.lock(); }
+	~lock_guard()             { mtx.unlock(); }
+};
 
 class Cursor
 {
@@ -15,17 +24,22 @@ private:
 	static Cursor* sinstance;
 	static std::mutex mtx;
 public:
+
+
 	static Cursor& get_instance()
 	{
-		mtx.lock();
+		lock_guard<std::mutex> g(mtx); 
+
+//		mtx.lock();
 
 		if (sinstance == nullptr)
 			sinstance = new Cursor;
-
-		mtx.unlock();
+//		mtx.unlock();
 
 		return *sinstance;
 	}
+
+
 };
 Cursor* Cursor::sinstance = nullptr;
 std::mutex Cursor::mtx;
