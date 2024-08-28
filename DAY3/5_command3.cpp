9840f7f9b4b0f2a6499e12c03b32df5c91217b6f
ecourse <smkang@codenuri.co.kr>
2024-08-28 14:30:29 +0900

    Add files via upload

--- DAY3/5_command3.cpp
+++ DAY3/5_command3.cpp
@@ -8,10 +8,19 @@ class BrightCommand
 {
 	Monitor& m;
 	int value;
+	int old_value;
 public:
 	BrightCommand(Monitor& m, int v) : m(m), value(v) {}
 
-	void execute() { m.set_brightness(value); }
+	void execute() 
+	{ 
+		old_value = m.get_brightness();
+		m.set_brightness(value);
+	}
+	void undo()
+	{
+		m.set_brightness(old_value);
+	}
 };
 
 int main()
@@ -19,9 +28,19 @@ int main()
 	Monitor m;
 
 	// #1. 그 일을 직접 하거나
-	m.set_brightness(90);
+
+	int old_value = m.get_brightness();
+
+	m.set_brightness(90);  // A
+
+	// 여기서 모니터 m 을 A의 실행 이전 상태로 복구해 보세요
+	m.set_brightness( ? );  
+
+
+
 
 	// #2. Command 패턴 사용
 	BrightCommand cmd(m, 90);
 	cmd.execute();
+	cmd.undo();
 }
