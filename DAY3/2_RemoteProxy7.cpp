227e2d5faefdf7cd9793c87b5d90776635dd98fc
ecourse <smkang@codenuri.co.kr>
2024-08-28 13:02:00 +0900

    Add files via upload

---
+++ DAY3/2_RemoteProxy7.cpp
@@ -0,0 +1,22 @@
+﻿#define USING_GUI
+#include "cppmaster.h"
+#include "ICalc.h"
+
+// raw pointer
+// => 객체가 아닙니다.
+// => 생성자/소멸자/복사생성자/대입연산자 개념이 없습니다.
+
+// proxy
+// => 다양한 이유로 대신할것을 만드는 패턴. 
+
+
+
+int main()
+{
+	ICalc* calc1 = load_proxy();
+}
+
+
+
+
+
