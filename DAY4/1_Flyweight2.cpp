5e174a9f980cbba6a9ab2b67d5b22f1a1ec046c9
ecourse <smkang@codenuri.co.kr>
2024-08-29 08:42:15 +0900

    Add files via upload

--- DAY4/1_Flyweight2.cpp
+++ DAY4/1_Flyweight2.cpp
@@ -6,20 +6,30 @@
 class Image
 {
 	std::string image_url;
-public:
+
 	Image(const std::string& url) : image_url(url)
 	{
 		std::cout << url << " Downloading..." << std::endl;
 	}
+public:
 	void Draw() { std::cout << "Draw " << image_url << std::endl; }
+
+
+	// 자신의 객체를 만드는 static 멤버 함수
+	static Image* Create(const std::string& url)
+	{
+		Image* img = new Image(url);
+		return img;
+	}
+
 };
 
 int main()
 {
-	Image* img1 = new Image("www.naver.com/a.png");
+	Image* img1 = Image::Create("www.naver.com/a.png");
 	img1->Draw();
 
-	Image* img2 = new Image("www.naver.com/a.png");
+	Image* img2 = Image::Create("www.naver.com/a.png");
 	img2->Draw();
 
 }
