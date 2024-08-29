a8b412a74d22219125f97f9adaa4fd6f987f2071
ecourse <smkang@codenuri.co.kr>
2024-08-29 14:50:48 +0900

    Add files via upload

--- DAY4/6_iterator5.cpp
+++ DAY4/6_iterator5.cpp
@@ -42,6 +42,11 @@ public:
 	{
 		return slist_iterator<T>(head); // 동적할당이 아닌 임시객체(값)로 반환
 	}
+
+	slist_iterator<T> end()
+	{
+		return slist_iterator<T>(0);
+	}
 };
 
 int main()
@@ -52,28 +57,43 @@ int main()
 	s.push_front(30);
 	s.push_front(40);
 
-	auto it = s.begin();
+	auto first = s.begin();
+	auto last = s.end();
 
-	std::cout << *it << std::endl; // 40
-	++it;
-		
-	std::cout << *it << std::endl;	// 30
+	while (first != last)
+	{
+		std::cout << *first << std::endl;
+		++first;
+	}
+
+
+//	auto it = s.begin();
+//	std::cout << *it << std::endl; // 40
+//	++it;
+//	std::cout << *it << std::endl;	// 30
 }
 
 
 
 
-// C++ 언어 관점에서 위 코드의 단점
+// STL 방식읜 위 코드는
 
 // #1. s.iterator() 가 반환한 반복자는 "new" 로 생성했다
 // => delete 해야 하지 않을까 ?
+
+// ===> s.begin() 에서 "new" 사용하지 않고, 값으로 반환!
+
 
 
 // #2. 다음으로 이동하는 함수 "next" 가 가상함수 입니다.
 // => 오버헤드 있습니다.
 // => 루프안에서 호출될수 있는 함수 이므로 성능이 중요
 
+// ===> operator++() 가상이 아닌 인라인 함수!! C와 동일 성능. 
+
 
 // #3. Raw Array 를 순환할때는 "raw pointer" 사용해서 ++로 이동합니다.
 // => 결국 이동 방식이 다르게 됩니다.
 // => 모든 "컬렉션을 동일한 방법" 에 위배 됩니다.
+
+// ===> operator++() ,operator*() 사용하므로 "포인터와 동일한 사용법"
