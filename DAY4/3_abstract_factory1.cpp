8e0610c6c657a71b33a25702f42dd8ec88e8761d
ecourse <smkang@codenuri.co.kr>
2024-08-29 13:05:26 +0900

    Add files via upload

---
+++ DAY4/3_abstract_factory1.cpp
@@ -0,0 +1,54 @@
+﻿// 3_추상팩토리 - 147
+#include <iostream>
+
+// 모든 종류의 컨트롤은 공통의 기반 클래스 필요
+struct IButton
+{
+	virtual void Draw() = 0;
+	virtual ~IButton() {}
+};
+struct IEdit
+{
+	virtual void Draw() = 0;
+	virtual ~IEdit() {}
+};
+
+
+struct WinButton : public IButton
+{
+	void Draw() { std::cout << "Draw WinButton" << std::endl; }
+};
+struct WinEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw WinEdit" << std::endl; }
+};
+
+struct OSXButton : public IButton
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+struct OSXEdit : public IEdit
+{
+	void Draw() { std::cout << "Draw OSXButton" << std::endl; }
+};
+
+int main(int argc, char** argv)
+{
+	// 각 스타일의 버튼의 공통의 기반 클래스 필요
+	IButton* btn;
+
+	if (strcmp(argv[1], "-style:OSX") == 0)
+		btn = new OSXButton;
+	else
+		btn = new WinButton;
+
+	// 그런데, 프로그램에서 "버튼"은 한개만 만들게 될까요 ?
+}
+
+
+
+
+
+
+
+
