04529be52f017c25e0ba56a6a863c5710cf99bf7
ecourse <smkang@codenuri.co.kr>
2024-08-29 15:58:04 +0900

    Add files via upload

--- DAY4/temporary3.cpp
+++ DAY4/temporary3.cpp
@@ -12,9 +12,16 @@ public:
 
 Point foo()
 {
-	Point pt(1, 2);
+//	Point pt(1, 2);
+//	return pt;	// 1. pt를 복사해서 리턴용 임시객체 만ㄴ들고
+				// 2. pt 는 파괴
+				// 3. 임시객체를 반환
 
-	return pt;
+	// 리턴용으로만 사용하는 객체는 
+	// "만들고 리턴하지 말고"
+	// "만들면서 리턴해라" - RVO ( return value optimization )
+
+	return Point(1, 2);// 리턴용 임시객체만 만들라는 의도!
 }
 
 int main()
