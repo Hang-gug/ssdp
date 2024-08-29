21e8f7ee2ab6df2bc62d9e6c140c099c3096d123
ecourse <smkang@codenuri.co.kr>
2024-08-29 15:43:58 +0900

    Add files via upload

--- DAY4/temporary1.cpp
+++ DAY4/temporary1.cpp
@@ -12,7 +12,20 @@ public:
 
 int main()
 {
-	Point pt(1, 1);
+//	Point pt(1, 1);	// named object 
+					// 이름이 있으므로 다음줄에서도 사용가능
+					// 수명 : {} 벗어 날때 파괴
+
+	Point (1, 1);	// unnamed object
+					// 수명 : 문장의 끝, 이름이 없으므로 다음 문장에서
+					//		  사용못함.
+					// "temporary  object(임시객체)"
 
 	std::cout << "------------\n";
 }
+
+
+
+
+
+
