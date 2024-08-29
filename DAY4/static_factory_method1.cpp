1ae1f226c397cb14c2749245671a25eeaebe4da4
ecourse <smkang@codenuri.co.kr>
2024-08-29 11:16:18 +0900

    Add files via upload

--- DAY4/static_factory_method1.cpp
+++ DAY4/static_factory_method1.cpp
@@ -4,9 +4,10 @@
 class Window
 {
 public:
-	Window() { init(); }
+//	Window() { init(); }
 
-	virtual void init() { std::cout << "Window init\n"; }
+	void foo() { init(); }
+	virtual void init() { std::cout << "1. Window init\n"; }
 };
 
 class MyWindow : public Window
@@ -14,11 +15,12 @@ class MyWindow : public Window
 	int data;
 public:
 	MyWindow() : data(99) {}
-	void init() override { std::cout << "MyWindow init\n"; int n = data; }
+	void init() override { std::cout << "2. MyWindow init\n"; int n = data; }
 };
 
 int main()
 {
 	MyWindow w;
+	w.foo();
 }
 // 실행하지 말고, 결과 예측해 보세요
