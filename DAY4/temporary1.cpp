fbabcc3825f7951052c6dff4637a918c9c44e6c9
ecourse <smkang@codenuri.co.kr>
2024-08-29 15:40:02 +0900

    Add files via upload

---
+++ DAY4/temporary1.cpp
@@ -0,0 +1,18 @@
+// temporary1
+#include <iostream>
+
+class Point
+{
+	int x, y;
+public:
+	Point(int x, int y) : x(x), y(y) {}
+
+	~Point() { std::cout << "~Point()\n"; }
+};
+
+int main()
+{
+	Point pt(1, 1);
+
+	std::cout << "------------\n";
+}
