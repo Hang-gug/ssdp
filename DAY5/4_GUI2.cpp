da4c098a84043187ac18fd67a38f2c1c82e7cb61
ecourse <smkang@codenuri.co.kr>
2024-08-30 09:52:25 +0900

    Add files via upload

--- DAY5/4_GUI2.cpp
+++ DAY5/4_GUI2.cpp
@@ -2,15 +2,7 @@
 #define USING_GUI
 #include "cppmaster.h"
 
-int foo(int hwnd, int msg, int a, int b)
-{
-	switch (msg)
-	{
-	case WM_LBUTTONDOWN: std::cout << "LBUTTON" << hwnd << std::endl; break;
-	case WM_KEYDOWN:     std::cout << "KEYDOWN" << hwnd << std::endl; break;
-	}
-	return 0;
-}
+
 
 class Window
 {
@@ -18,6 +10,21 @@ class Window
 public:
 	void create(const std::string& title)
 	{
+		handle = ec_make_window(&msgproc, title);
+	}
+
+	// 핵심 #1. 아래 함수가 static 멤버 함수 이어야 하는 이유를 알아야 합니다.
+	// => 다른 함수의 인자로 전달되는 함수(C 언어의 callback 함수)를
+	//    클래스로 만들때는 "static 멤버로 해야 합니다."
+
+	static int msgproc(int hwnd, int msg, int a, int b)
+	{
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN: std::cout << "LBUTTON" << hwnd << std::endl; break;
+		case WM_KEYDOWN:     std::cout << "KEYDOWN" << hwnd << std::endl; break;
+		}
+		return 0;
 	}
 };
 
