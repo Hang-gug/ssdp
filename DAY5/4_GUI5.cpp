9e0d87bc37be9a9e85ba570da1683c2dfc7ea1f1
ecourse <smkang@codenuri.co.kr>
2024-08-30 10:13:00 +0900

    Add files via upload

---
+++ DAY5/4_GUI5.cpp
@@ -0,0 +1,81 @@
+#define USING_GUI
+#include "cppmaster.h"
+#include <map>
+#include <vector>
+
+class Window;
+
+std::map<int, Window*> this_map;
+
+class Window
+{
+	int handle;
+
+	Window* parent = nullptr;
+
+	std::vector<Window*> child_vector;
+public:
+
+	void add_child(Window* child)
+	{
+		child->parent = this;
+
+		child_vector.push_back(child);
+
+		ec_add_child(this->handle, child->handle);
+	}
+
+
+	void create(const std::string& title)
+	{
+		handle = ec_make_window(&msgproc, title);
+
+		this_map[handle] = this;
+	}
+
+
+	static int msgproc(int hwnd, int msg, int a, int b)
+	{
+		Window* self = this_map[hwnd];
+
+		switch (msg)
+		{
+		case WM_LBUTTONDOWN:
+			self->lbutton_down();
+			break;
+
+		case WM_KEYDOWN:
+			self->key_down();
+			break;
+		}
+		return 0;
+	}
+
+	virtual void lbutton_down() {}
+	virtual void key_down() {}
+};
+
+class MainWindow : public Window
+{
+public:
+	void lbutton_down() override { std::cout << "MainWindow lbutton_down\n"; }
+};
+class ImageView : public Window
+{
+public:
+	void lbutton_down() override { std::cout << "ImageView lbutton_down\n"; }
+};
+int main()
+{
+	MainWindow w1;
+	w1.create("MainWindow");
+
+	ImageView w2;
+	w2.create("ImageView");
+
+
+	w1.add_child(&w2); 
+
+	ec_process_message();
+}
+
